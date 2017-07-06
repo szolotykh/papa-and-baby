8ce50160d4615af7322d805b7cb5b7d6706bfb32
Sergey Zolotykh <milk3dfx@gmail.com>
2017-07-05 22:15:26 -0400

    Add validation to info object.

--- js/controllers/display-controller.js
+++ js/controllers/display-controller.js
@@ -1,21 +1,35 @@
 angular.module('PapaAndBaby')
 .controller('displayCtrl', ['$scope', '$routeParams', '$window',  function($scope, $routeParams, $window) {
+
     var params = Object.keys($routeParams);
     if(params.length == 1){
         try{
             var info = JSON.parse(atob(params[0]));
+            if (info.l1.length == 0 || info.l1.length > 50 ||
+                info.l2.length == 0 || info.l2.length > 50 ||
+                info.d.length == 0)
+                {
+                throw "Validation error.";
+                }
+            $scope.lover1 = info.l1;
+            $scope.lover2 = info.l2;
+            
+            var startDate = new Date(info.d);
+            if (startDate == "Invalid Date")
+                {
+                throw "Validation error.";
+                }
+            var currentDate = new Date();
+            var days = Math.ceil ((currentDate-startDate)/86400000);
+            if(days < 0 || days > 100000)
+                {
+                throw "Validation error.";
+                }
+            $scope.days = days + " Days";
         }
         catch (e){
             $window.location.href = '#/setup/';
         }
-        console.log (info);
-        $scope.lover1 = info.l1;
-        $scope.lover2 = info.l2;
-
-        var start = new Date(info.d);
-        var current = new Date();
-        var days = Math.round((current-start)/86400000);
-        $scope.days = days + " Days";
     }
     else{
         $window.location.href = '#/setup/';
